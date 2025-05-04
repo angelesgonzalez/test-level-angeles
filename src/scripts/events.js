abf3c12642d17ed57dfb2ca5870b690648595940
angelesgonzalez <delosangelesgonzalez@gmail.com>
2025-05-04 11:20:43 +0200

    Edit form Event listener to use try catch on the main logic and catch errors

--- src/scripts/events.js
+++ src/scripts/events.js
@@ -4,11 +4,29 @@ import { rounds } from "./rounds.js";
 
 form.addEventListener("submit", (event) => {
 	event.preventDefault();
+	let numberInput = 0;
 	try {
-	} catch (error) {}
-	let numberInput = +document.getElementById("number-input").value;
-	let result = fizzBuzz(numberInput);
-	rounds.push(result);
-	updateHTML(result);
+		numberInput = document.getElementById("number-input").value;
+		const parsedNumber = Number(numberInput);
+
+		if (isNaN(parsedNumber)) {
+			throw new Error("Not a valid number");
+		}
+
+		let result = fizzBuzz(parsedNumber);
+
+		updateHTML(result);
+
+		if (result.status === "Ok") {
+			rounds.push(result);
+		}
+	} catch (error) {
+		updateHTML({
+			status: "Error",
+			message: "There's an error. Please introduce only numbers.",
+			data: { input: numberInput, output: "Error" },
+		});
+	}
+
 	clearAllInput();
 });
